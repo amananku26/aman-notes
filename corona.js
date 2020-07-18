52a935c9954bc2f2b5842c21030ec9f35cd9d783
amananku26 <amankumaranku@gmail.com>
2020-07-18 08:17:14 +0530

    Update Functionality to Access the data of Covid-19

--- corona.js
+++ corona.js
@@ -17,7 +17,51 @@ window.onload = function() {
 
 function handleRespone(response) {
     renderDom(response.Countries)
+    console.log(response.Countries[0].Country)
+
+    var pp = document.getElementById('vbtn')
+    pp.addEventListener('click', function() {
+        var name = document.getElementById('country1').value
+        console.log(name)
+        var show1 = document.getElementById('cname')
+        var nc = document.getElementById('nc')
+        var nd = document.getElementById('nd')
+        var nr = document.getElementById('nr')
+        var tc = document.getElementById('tc')
+        var td = document.getElementById('td')
+        for (var i = 0; i < response.Countries.length; i++) {
+            if (response.Countries[i].Country == name) {
+                console.log(typeof response.Countries[i].Country)
+                show1.innerHTML = response.Countries[i].Country
+                nc.innerHTML = response.Countries[i].NewConfirmed
+                nd.innerHTML = response.Countries[i].NewDeaths
+                nr.innerHTML = response.Countries[i].NewRecovered
+                tc.innerHTML = response.Countries[i].TotalConfirmed
+                td.innerHTML = response.Countries[i].TotalDeaths
+                    // + "<br/>" +
+                    //     "New Confirmed--" + " " + response.Countries[i].NewConfirmed + "<br/>" +
+                    //     "New Deaths--" + " " + response.Countries[i].NewDeaths + "<br/>" +
+                    //     "New Recovered--" + " " + response.Countries[i].NewRecovered + "<br/>" +
+                    //     "Total Confirmed--" + " " + response.Countries[i].TotalConfirmed + "<br/>" +
+                    //     "Total Deaths--" + " " + response.Countries[i].TotalDeaths
+                    // show1.style.border = "2px solid #0000F";
+                    // show1.style.border = "2px solid #808080";
+                    // show1.style.padding = "20px";
+                    // show1.style.marginBottom = "20px";
+            }
+        }
+    })
+
+
 }
+
+
+// function okkk() {
+//     var name = document.getElementById('country1').value
+//     console.log(name)
+
+// }
+
 
 function renderDom(arr) {
     var result = document.getElementById('result')
@@ -27,7 +71,10 @@ function renderDom(arr) {
 
     for (var i = 0; i < arr.length; i++) {
         var div3 = document.createElement('div')
-        div3.innerHTML = "Country:" + " " + arr[i].Country + "<br/>" + "New Confirmed:" + " " + arr[i].NewConfirmed + "<br/> " + " Total Confirmed:" + " " + arr[i].TotalConfirmed + " <br/>" + " Total Deaths:" + " " + arr[i].TotalDeaths + "<br/>"
+        div3.innerHTML = "Country:" + " " + arr[i].Country + "<br/>" +
+            "New Confirmed:" + " " + arr[i].NewConfirmed + "<br/> " + " Total Confirmed:" +
+            " " + arr[i].TotalConfirmed + " <br/>" + " Total Deaths:" + " " + arr[i].TotalDeaths +
+            "<br/>"
         ul.style.border = "2px solid #0000F";
         div3.style.border = "2px solid #808080";
         div3.style.padding = "20px";
